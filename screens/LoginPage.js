ddc82dce3bdc92d914e97018e4d818a2b831970f
EDK <doganertankaya@gmail.com>
2021-10-06 15:21:24 +0300

    oke

--- screens/LoginPage.js
+++ screens/LoginPage.js
@@ -2,179 +2,225 @@ import { placeholder } from "@babel/types";
 import * as React from "react";
 import { useState } from "react";
 import {
-    StyleSheet,
-    Image,
-    View,
-    Text,
-    StatusBar,
-    TouchableOpacity,
-    ImageBackground,
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+  ImageBackground,
+  Platform,
+  KeyboardAvoidingView,
 } from "react-native";
 
 import { TextInput, Button, Checkbox } from "react-native-paper";
-import { SocialIcon } from 'react-native-elements';
-
+import { SocialIcon } from "react-native-elements";
 
 // DÜz fetch değil axios kullan!!!
 
 const getPost = () => {
-    return fetch("https://reactnative.dev/movies.json")
-        .then((response) => response.json())
-        .then((json) => {
-            if (json.movies[0].id == 1) {
-                console.log("Okey giriş yap")
-            }
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+  return fetch("https://reactnative.dev/movies.json")
+    .then((response) => response.json())
+    .then((json) => {
+      if (json.movies[0].id == 1) {
+        console.log(Platform.OS);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 const Login = () => {
-    const [checked, setChecked] = useState(false)
+  const [checked, setChecked] = useState(false);
 
-    return (
-        <>
-            <View style={styles.container}>
-                <View style={styles.Logo}>
-                    <Image
-                        source={{
-                            uri: "https://hdsotomasyon.com/wp-content/uploads/2018/04/logo-mavi.png",
-                        }}
-                        style={{ width: 230, height: 70 }}
-                    />
-                </View>
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.Logo}>
+          <Image
+            source={{
+              uri: "https://hdsotomasyon.com/wp-content/uploads/2018/04/logo-mavi.png",
+            }}
+            style={{ width: 230, height: 70 }}
+          />
+        </View>
 
-                <View>
-                    <TextInput
-                        style={styles.input}
-                        mode="outlined"
-                        label="E-mail"
-                        theme={{ roundness: 10, colors: { primary: "#067dc1" } }}
-                    />
-                    <View style={styles.borderStyle}>
-                        <TextInput
-                            style={styles.input}
-                            mode="outlined"
-                            label="Şifre"
-                            theme={{ roundness: 10, colors: { primary: "#067dc1" } }}
-                        />
-                    </View>
-                    <TouchableOpacity
-                        onPress={() => {
-                            setChecked(!checked);
-                        }}
-                    >
-                        <View
-                            style={{
-                                marginLeft: 20,
-                                flexDirection: "row",
-                                alignItems: "center",
-                            }}
-                        >
-                            <Checkbox
-                                color="#067dc1"
-                                status={checked ? "checked" : "unchecked"}
-                            />
-                            <Text>Beni Hatırla</Text>
-                        </View>
-                        
-                    </TouchableOpacity>
-                    <View style={styles.GirisYapButton}>
-                        <Button
-                            icon="login"
-                            mode="contained"
-                            color="#067dc1"
-                            style={{ height: 50, justifyContent: "center", borderRadius: 10 }} // justifyContent: "center" ortalar.
-                            contentStyle={{ height: 50, justifyContent: "center" }} // eğer bunu eklemezsen butona tıklayınca tıklama alanın kücük gözükür.
-                            onPress={getPost}
-                        >
-                            Giriş Yap
-                        </Button>
-                        <View style={{ alignItems: "center", paddingTop: 20 }}>
+        <View>
+          <TextInput
+            style={Platform.OS === "web" ? styles.inputweb : styles.input}
+            mode="outlined"
+            label="E-mail"
+            theme={{ roundness: 10, colors: { primary: "#067dc1" } }}
+          />
+          <View style={styles.borderStyle}>
+            <TextInput
+              style={Platform.OS === "web" ? styles.inputweb : styles.input}
+              mode="outlined"
+              label="Şifre"
+              theme={{ roundness: 10, colors: { primary: "#067dc1" } }}
+            />
+          </View>
 
-                            <Text
-                                style={{
-                                    justifyContent: "center",
-                                    color: "#067dc1",
-                                }}
-                            >
-                                Şifremi UnuttumTest
-                            </Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={{ position: 'absolute', left: 0, right: 0, bottom: 0 }}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center', }}>
-                        <View style={{ flex: 1, height: 1, backgroundColor: '#f0f0f0', marginTop: 9, }} />
-                        <Text
-                            style={{
-                                color: "#067dc1",
-                                marginTop: 15,
-                                marginBottom: 5,
-                                width: 175,
-                                textAlign: 'center'
-                            }}
-                        >
-                            Sosyal Ağ ile Oturum Aç
-                        </Text>
-                        <View style={{ flex: 1, height: 1, backgroundColor: '#f0f0f0', marginTop: 9, }} />
-                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        </View>
-                    </View>
-                    <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
-                        <SocialIcon
-                            style={{ marginLeft: 10 }}
-                            type='facebook'
-                        />
-                        <SocialIcon
-                            style={{ marginLeft: 20 }}
-                            type='google'
-                        />
-                        <SocialIcon
-                            style={{ marginLeft: 20 }}
-                            type='twitch'
-                        />
-                    </View>
-                </View>
+          <View
+            style={
+              Platform.OS === "web"
+                ? {
+                    alignSelf: "center",
+                    width: "40%",
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }
+                : {
+                    marginLeft: 20,
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }
+            }
+          >
+            <TouchableOpacity
+              onPress={() => {
+                setChecked(!checked);
+              }}
+            >
+              <Checkbox
+                color="#067dc1"
+                status={checked ? "checked" : "unchecked"}
+              />
+            </TouchableOpacity>
+
+            <Text>Beni Hatırla</Text>
+          </View>
+
+          <View
+            style={
+              Platform.OS === "web"
+                ? styles.GirisYapButtonWeb
+                : styles.GirisYapButton
+            }
+          >
+            <Button
+              icon="login"
+              mode="contained"
+              color="#067dc1"
+              style={{ height: 50, justifyContent: "center", borderRadius: 10 }} // justifyContent: "center" ortalar.
+              contentStyle={{ height: 50, justifyContent: "center" }} // eğer bunu eklemezsen butona tıklayınca tıklama alanın kücük gözükür.
+              onPress={getPost}
+            >
+              Giriş Yap
+            </Button>
+            <View style={{ alignItems: "center", paddingTop: 20 }}>
+              <Text
+                style={{
+                  justifyContent: "center",
+                  color: "#067dc1",
+                }}
+              >
+                Şifremi Unuttum
+              </Text>
             </View>
-        </>
-    );
+          </View>
+        </View>
+
+        <View style={{ position: "absolute", left: 0, right: 0, bottom: 0 }}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <View
+              style={{
+                flex: 1,
+                height: 1,
+                backgroundColor: "#f0f0f0",
+                marginTop: 9,
+              }}
+            />
+            <Text
+              style={{
+                color: "#067dc1",
+                marginTop: 15,
+                marginBottom: 5,
+                width: 175,
+                textAlign: "center",
+              }}
+            >
+              Sosyal Ağ ile Oturum Aç
+            </Text>
+            <View
+              style={{
+                flex: 1,
+                height: 1,
+                backgroundColor: "#f0f0f0",
+                marginTop: 9
+              }}
+            />
+            <View style={{ flexDirection: "row", alignItems: "center" }}></View>
+          </View>
+          <View style={{ flexDirection: "row", justifyContent: "center" }}>
+            <SocialIcon style={{ marginLeft: 10 }} type="facebook" />
+
+              {Platform.OS == "android" ?
+                 <SocialIcon style={{ marginLeft: 20 }} type="google" />
+                  : Platform.OS == "ios" ?
+                  <SocialIcon light style={{ marginLeft: 20 }} iconColor='#000'  type='apple' />
+                  :
+                  <SocialIcon style={{ marginLeft: 20 }} type="google" />
+            }
+
+            <SocialIcon style={{ marginLeft: 20 }} type="instagram" />
+          </View>
+          
+        </View>
+      </View>
+    </>
+  );
 };
 
 export default Login;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        //  alignItems: "center",
-        justifyContent: "center",
-        zIndex: 1
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    //  alignItems: "center",
+    justifyContent: "center",
+    zIndex: 1,
+  },
 
-    Logo: {
-        alignItems: "center",
-        paddingTop: 40
-    },
+  Logo: {
+    alignItems: "center",
+    paddingTop: 40,
+  },
 
-    input: {
-        height: 50,
-        marginRight: 20,
-        marginLeft: 20,
-        padding: 7,
-        backgroundColor: "#fff",
-    },
+  input: {
+    height: 50,
+    marginRight: 20,
+    marginLeft: 20,
+    padding: 7,
+    backgroundColor: "#fff",
+  },
 
-    GirisYapButton: {
-        margin: 5,
-        marginRight: 20,
-        marginLeft: 20,
-        padding: 10,
-    },
+  inputweb: {
+    height: 50,
+    alignSelf: "center",
+    width: "40%",
+    padding: 7,
+    backgroundColor: "#fff",
+  },
 
-    borderStyle: {
-        borderRadius: 25,
-        overflow: "hidden",
-    },
+  GirisYapButton: {
+    margin: 5,
+    marginRight: 20,
+    marginLeft: 20,
+    padding: 10,
+  },
+
+  GirisYapButtonWeb: {
+    margin: 5,
+    alignSelf: "center",
+    width: "40%",
+    padding: 10,
+  },
+
+  borderStyle: {
+    borderRadius: 25,
+    overflow: "hidden",
+  },
 });
